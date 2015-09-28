df7462892512459cf6d510f94e1f47e7b9780b5c
aml7785 <ambermlyon@hotmail.com>
2015-09-28 12:53:04 -0500

    Displaying created and modified dates

--- plattevillenews/posts/admin.py
+++ plattevillenews/posts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import News
-# Register your models here.
 
-admin.site.register(News)
+# Register your models here.
+class NewsAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_date', 'modified_date',)
+    
+admin.site.register(News, NewsAdmin)
+
+
